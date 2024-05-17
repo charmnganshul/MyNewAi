b8aa62dd07304294a75bede7f0087d4b7d1c12ea
Anshul <anshulchanpuria18@gmail.com>
2024-05-17 16:57:30 +0530

    first commit

---
+++ gemAi/src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from './components/sidebar/sidebar'
+import Main from './components/sidebar/main/main'
+const App = () => {
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+
+    </>
+  )
+}
+
+export default App
